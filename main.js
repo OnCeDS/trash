c8d1af5c0f453f47719b8f2cfc336eb441cb19bc
Sergey <57293178+OnCeDS@users.noreply.github.com>
2020-04-21 19:46:33 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,69 @@
+var category = ["Gaming PC",
+                "Monoblocks",
+                "Laptops"];
+
+var name = ["Gaming Plus Ultra Daily Deal",
+            "Intel Creator Daily Deal",
+            "Intel Z390 mATX Gaming PC EX",
+            "Intel Z390 Elixir",
+            "Apple iMac 21.5",
+            "Lenovo IdeaCentre AIO A340-22",
+            "Lenovo IdeaCentre A540",
+            "ASUS TUF505DU-KB71 Gaming Laptop",
+            "MSI GF65 THIN 9SEXR-250 Gaming Laptop",
+            "Chimera NH58DDW (NP7858DW) Gaming Laptop"];
+
+var src_imgLink = ["src\PC-I9(0).png",
+                   "src\PC-I7(1).png",
+                   "src\PC-I7(2).png",
+                   "src\PC-I9(3).png",
+                   "src\PC-IMAC(4).png",
+                   "src\PC-MONO(5).jpg",
+                   "src\PC-MONO(6).png",
+                   "src\PC-MoLAP(7).png",
+                   "src\PC-MoLAP(8).png",
+                   "src\PC-MoLap(9).png"];
+
+var cpu = ["Intel® Core™ i9-9900K",
+           "Intel® Core™ i7-9700K",
+           "Intel® Core™ i7-9700K",
+           "Intel® Core™ i9-9900K",
+           "Intel® Core™ i5",
+           "Intel® Pentium™ 5405U",
+           "Intel® Core™ i7-8700T",
+           "AMD Ryzen 7 3750H",
+           "Intel® Core™ i7-9750H",
+           "Intel® Core™ i7-10875H"];
+
+var videoCard = ["NVIDIA GeForce RTX 2080 Ti - 11GB GDDR6",
+                 "NVIDIA GeForce RTX 2080 SUPER - 8GB GDDR6",
+                 "NVIDIA GeForce RTX 2070 SUPER - 8GB GDDR6",
+                 "NVIDIA GeForce RTX 2080 Ti - 11GB GDDR6"
+                 "Intel® Iris Plus Graphics 640",
+                 "Intel UHD Graphics 610",
+                 "AMD Radeon™ RX 560X",
+                 "NVIDIA GeForce GTX 1660 Ti 6GB GDDR6",
+                 "NVIDIA GeForce RTX 2060 6GB GDDR6",
+                 "NVIDIA GeForce RTX 2060 6GB GDDR6"];
+
+var cost = ["2049$",
+            "1709$",
+            "1699$",
+            "3499$",
+            "1234$",
+            "324$",
+            "1034$",
+            "946$",
+            "1149$",
+            "1399$"];
+
+var contact = ["+79099310811",
+               "+79034329812",
+               "+79023456789",
+               "+79016789012",
+               "+79097853456",
+               "+79093224567",
+               "+79052347890",
+               "+79046572165",
+               "+79019043412",
+               "+79017890934"];
